246add0f14b67997785cdd36164e2e203851cb0c
KMV <9117479@gmail.com>
2022-04-21 15:09:19 +0300

    NC Navigation View Lesson31

--- app/src/main/java/com/example/drawerlayout/MainActivity.kt
+++ app/src/main/java/com/example/drawerlayout/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.drawerlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.view.GravityCompat
 import com.example.drawerlayout.databinding.ActivityMainBinding
 
@@ -12,6 +13,26 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.apply {
+            menuNavLeft.setNavigationItemSelectedListener {
+                when(it.itemId){
+                    R.id.app_info -> {
+                        Toast.makeText(this@MainActivity, "Learn is light", Toast.LENGTH_SHORT).show()
+                    }
+                    R.id.app_docs -> {
+                        Toast.makeText(this@MainActivity, "Рукописи не горят", Toast.LENGTH_SHORT).show()
+                    }
+                    R.id.app_reports -> Toast.makeText(this@MainActivity, "Don`t worry, be happy", Toast.LENGTH_SHORT).show()
+                    R.id.app_pay -> {
+
+                    }
+                    R.id.app_account -> Toast.makeText(this@MainActivity, "SuperMan owns this Account", Toast.LENGTH_SHORT).show()
+                    R.id.app_settings -> {
+
+                    }
+                }
+                drawerL.closeDrawer(GravityCompat.START)
+                true
+            }
             buttonOpenMenu.setOnClickListener {
                 drawerL.openDrawer(GravityCompat.START)
             }
